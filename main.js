dca85864c476b332a12634d1b505b2ac5ce51ec7
Ivan Alvarez <ivan.alvarez.r@gmail.com>
2014-02-11 13:58:40 -0500

    finished minimun requirements

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var net = require('net');
 var __ = require('underscore');
  
 var users = [];
-var rooms = [{name: 'default', users: []}];
+var rooms = [];
 
 //User statuses
 var READY = 1;
@@ -47,7 +47,11 @@ function onData(socket, data) {
 	} 
 	// else send text to the room
 	else{
-		sendMessage(user.room, user.nickname + ": " + data );
+		if (user.room){
+			sendMessage(user.room, user.nickname + ": " + data );
+		}else{
+			socket.write("Join a room first using /join or see a list of the active rooms with /rooms\n");
+		}		
 	}
 	
 }
@@ -65,7 +69,6 @@ function assignNickname(socket, nickname){
 		socket.write('Login name?\n');
 	}else{
 		user.nickname = nickname;
-		joinRoom('default', user);
 		user.status = READY;
 		socket.write('Welcome ' + nickname + '!\n');
 	}
@@ -75,15 +78,27 @@ function assignNickname(socket, nickname){
  * Executes a /xxx command
  */
 function executeCommand(socket, command){
-	switch(command){
+	var args = command.split(" ");
+	switch(args[0]){
 		case '/quit':
 			socket.end('BYE\n');
 			break;
 		case '/rooms':
 			socket.write('Active rooms are:\n');
 			__.each(rooms, function (room){
-				socket.write('* ' + room.name + ' (' + room.users.length + ')');
+				socket.write('* ' + room.name + ' (' + room.users.length + ')\n');
 			});
+			socket.write("end of list.\n");
+			break;
+		case '/join':
+			if (args[1]) {
+				joinRoom(args[1], socket.user);
+			}else {
+				socket.write("Specify a room to join. e.g: /join games\n");
+			}
+			break;
+		case '/leave':
+			leaveRoom(socket.user);
 			break;
 		default:
 			socket.write('Unrecognized command\n');
@@ -91,7 +106,7 @@ function executeCommand(socket, command){
 }
 
 /*
- * Sends a message to all the users in a room
+ * Sends a message to all the users in a room. 
  */
 function sendMessage(room, message){
 	__.each(room.users, function(user){
@@ -100,13 +115,26 @@ function sendMessage(room, message){
 }
 
 /*
+ * Sends a message to all the users in a room except the sender. 
+ */
+function sendMessageFiltered(room, message, sender){
+	__.each(room.users, function(user){
+		if(sender != user){
+			user.socket.write(message + '\n');
+		}
+	});
+}
+
+/*
  * Add the user to a chat room
  */
 function joinRoom(roomName, user){
-	if(!rooms[roomName]){
-		rooms[roomName] = {name: roomName, users:[]};
+	var room = __.find(rooms, function (iroom){return iroom.name == roomName;});
+	if(!room){
+		room = {name: roomName, users:[]};
+		rooms.push(room);
 	}
-	user.room = rooms[roomName];
+	user.room = room;
 	user.room.users.push(user);
 	user.socket.write("entering room: " + roomName + '\n');
 	__.each(user.room.users, function(iuser){
@@ -117,6 +145,21 @@ function joinRoom(roomName, user){
 		user.socket.write("\n");
 	});
 	user.socket.write("end of list.\n");
+	sendMessageFiltered(room, "* new user joined " + roomName + ": " + user.nickname, user);
+}
+
+/*
+ * Remove the user from a chat room
+ */
+function leaveRoom(user){
+	var room = user.room;
+	room.users.splice(room.users.indexOf(user), 1);
+	if (room.users.length == 0){
+		rooms.splice(rooms.indexOf(room), 1);
+	}
+	user.room = null;
+	sendMessageFiltered(room, "* user has left " + room.name + ": " + user.nickname);
+	user.socket.write("* user has left " + room.name + ": " + user.nickname + " (** this is you)\n");
 }
 
 /*
